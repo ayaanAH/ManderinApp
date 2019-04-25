798b48a75cffc0d5370d452bd0afefeccb30d476
Ayaan Khan <49956381+ayaanAH@users.noreply.github.com>
2019-04-25 21:56:45 +0530

    new family images

--- app/src/main/java/com/example/manderinapp/FamilyMembers.java
+++ app/src/main/java/com/example/manderinapp/FamilyMembers.java
@@ -19,15 +19,15 @@ public class FamilyMembers extends AppCompatActivity
         setContentView(R.layout.activity_family_members);
         familyList=new ArrayList<>();
 
-        familyList.add(new Word(R.drawable.father,"Father","父亲"));
-        familyList.add(new Word(R.drawable.mother,"Mother","妈妈"));
-        familyList.add(new Word(R.drawable.son,"Son","儿子"));
-        familyList.add(new Word(R.drawable.daughter,"Daughter","女儿"));
-        familyList.add(new Word(R.drawable.brother,"Younger brother","弟弟"));
-        familyList.add(new Word(R.drawable.sister,"Younger sister","妹妹"));
-        familyList.add(new Word(R.drawable.grandmother,"Grandmother","姥姥"));
-        familyList.add(new Word(R.drawable.grandfather,"Grandfather","爷爷"));
-        familyList.add(new Word(R.drawable.elderbrother,"Elder brother","哥哥"));
+        familyList.add(new Word(R.drawable.family_father,"Father","父亲"));
+        familyList.add(new Word(R.drawable.family_mother,"Mother","妈妈"));
+        familyList.add(new Word(R.drawable.family_son,"Son","儿子"));
+        familyList.add(new Word(R.drawable.family_daughter,"Daughter","女儿"));
+        familyList.add(new Word(R.drawable.family_younger_brother,"Younger brother","弟弟"));
+        familyList.add(new Word(R.drawable.family_younger_sister,"Younger sister","妹妹"));
+        familyList.add(new Word(R.drawable.family_grandmother,"Grandmother","姥姥"));
+        familyList.add(new Word(R.drawable.family_grandfather,"Grandfather","爷爷"));
+        familyList.add(new Word(R.drawable.family_older_brother,"Elder brother","哥哥"));
 
         familyListView=findViewById(R.id.familiesLists);
         WordAdapter wrdAdapt=new WordAdapter(this,familyList,R.color.redback);
